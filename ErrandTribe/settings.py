547149e72bd6f97b0d50b74f19a64f44166e09f5
akinolablessing <akinolablessing1890@gmail.com>
2025-10-13 17:23:50 +0100

    I fixed slach error

--- ErrandTribe/settings.py
+++ ErrandTribe/settings.py
@@ -169,7 +169,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://errand-tribe.vercel.app/",
+    "https://errand-tribe.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOW_ALL_ORIGINS = True
