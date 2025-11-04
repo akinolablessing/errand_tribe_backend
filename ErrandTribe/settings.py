9c235a2865dea5be4b07f86606fbbf1bdbb75a67
akinolablessing <akinolablessing1890@gmail.com>
2025-11-04 16:08:37 +0100

    change the database

--- ErrandTribe/settings.py
+++ ErrandTribe/settings.py
@@ -96,6 +96,9 @@ DATABASES = {
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
+        'OPTIONS': {
+            'sslmode': 'require',   # <--- MUST be present
+        },
     }
 }
 
